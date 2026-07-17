75545933e6f0db21570b317fe8e0ad8286c84914
Prachi lembhe <prachi2003nov04@gmail.com>
2026-07-17 17:16:41 +0530

    update layout component to improve metadata description and body structure

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,18 @@
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "Buy Me a Coffee ☕",
-  description: "Support creators with a coffee using Razorpay",
+  description: "Support my work with a coffee",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gradient-to-br from-yellow-50 via-orange-50 to-amber-100 min-h-screen text-gray-900">
+      <body>
         {children}
+
+        <Script src="https://checkout.razorpay.com/v1/checkout.js" />
       </body>
     </html>
   );
